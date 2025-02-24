08c9423d11b916c1132a37a45d9d2725f40de819
eazyposy <thakur.raxit@gmail.com>
2025-02-24 19:53:44 +0530

    iframe addition

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,12 @@ import AuthButton from "@/components/AuthButton";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <AuthButton />
+    <div className="flex flex-col items-center justify-center">
+    <div className="mt-10">
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqDrXafqmYEztKVcjrYlGWsJE4Y7ugJCamlJtA9kUXTeYDHg/viewform?embedded=true" width="640" height="722" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
+      <AuthButton />
+      </div>
+      </div>
   );
 }
