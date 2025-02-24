6c392094104728784fb6840edf2c9d4d3cbc58a7
eazyposy <thakur.raxit@gmail.com>
2025-02-24 19:56:32 +0530

    New Form Commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,9 @@ import Image from "next/image";
 import AuthButton from "@/components/AuthButton";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div className="flex flex-col items-center justify-center">
-    <div className="mt-10">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqDrXafqmYEztKVcjrYlGWsJE4Y7ugJCamlJtA9kUXTeYDHg/viewform?embedded=true" width="640" height="722" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-    </div>
       <AuthButton />
-      </div>
       </div>
   );
 }
